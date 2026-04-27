994ad54bbee377df371dfb3ad7a982be379d64f2
farhan-al-shahrier-siam <farhanalshahriersiam@gmail.com>
2026-04-27 17:37:20 +0600

    created and connected mongoDB and data can be stored in the database

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,12 +2,18 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("mongodb://localhost:27017/database");
-const db = client.db();
+const client = new MongoClient(process.env.AUTH_DB_URI);
+
+// connect to DB
+await client.connect();
+
+const db = client.db("better-auth-db");
 
 export const auth = betterAuth({
-  database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
-    client
-  }),
+    emailAndPassword: {
+        enabled: true,
+    },
+    database: mongodbAdapter(db, {
+        client,
+    }),
 });
